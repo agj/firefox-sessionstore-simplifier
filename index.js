c954857b651124f96b0b1cf5d4270094654ceac5
agj <ale@agj.cl>
2016-05-24 12:50:19 +0900

    Made it take any number of files named anything.

--- index.js
+++ index.js
@@ -2,17 +2,21 @@
 import R from 'ramda'
 import fs from 'fs-promise'
 import dotInto from 'dot-into'
+import glob from 'glob-promise'
 
 dotInto.install();
 
 const all = Promise.all.bind(Promise);
+const allObj = ps =>
+	all(R.values(ps))
+	.then(R.zipObj(R.keys(ps)));
 
 const simplify = o => ({ url: o.url, title: o.title });
 
-['input/current.json',
- 'input/old.json']
-.map(file => fs.readFile(file, { encoding: 'utf8' }))
-.into(all)
+glob('input/*')
+.then(R.reduce((acc, file) => { acc[file] = file; return acc }, {}))
+.then(R.map(file => fs.readFile(file, { encoding: 'utf8' })))
+.then(allObj)
 .then(R.map(JSON.parse))
 .then(R.map(data =>
 	data.windows
@@ -20,13 +24,12 @@ const simplify = o => ({ url: o.url, title: o.title });
 	.into(R.flatten)
 ))
 .then(R.map(R.map(R.prop('entries'))))
-.then(R.tap(console.log))
 .then(R.map(R.map(R.last)))
 .then(R.map(R.map(simplify)))
 .then(R.map(data => JSON.stringify(data, null, '\t')))
-.then(([current, old]) => {
-	fs.writeFile('output/current.json', current, { encoding: 'utf8' });
-	fs.writeFile('output/old.json', old, { encoding: 'utf8' });
-})
+.then(jsons =>
+	R.keys(jsons)
+	.forEach(f => fs.writeFile('output/' + R.replace(/input\/(.+)/g, '$1', f), jsons[f], { encoding: 'utf8' }))
+)
 .catch(console.error)
 
